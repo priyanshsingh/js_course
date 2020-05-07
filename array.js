2dd683915a206a45917395a71980a5b2eb59efd7
Priyansh Singh <priyanshsingh1401@gmail.com>
2020-05-07 11:12:55 +0000

    added array.js file

---
+++ array.js
@@ -0,0 +1,8 @@
+// storing names of various car brand in an array and then printing them using for loop in the console...
+
+var cars = ['Lamborghini','Bugatti','Mercedes','Volvo','BMW','Audi']
+
+for(i=0; i<cars.length; i++)
+{
+  console.log(cars[i]);
+}
