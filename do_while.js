b1680711679029b798edfdcade52637c072c6078
Priyansh Singh <priyanshsingh1401@gmail.com>
2020-05-07 08:59:53 +0000

    Modified do_while.js file

--- do_while.js
+++ do_while.js
@@ -1,2 +1,9 @@
-var x = 231
-  console.log(x);
+// print your name as many times you need using Do While Loop.
+ 
+ var x = 1  
+  do
+  {
+    console.log("Priyansh");
+    x++
+  }
+    while(x<5)
