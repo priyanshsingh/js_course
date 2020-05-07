f8194563edc11cd30c582e10a4f5a7ca7792ad0b
Priyansh Singh <priyanshsingh1401@gmail.com>
2020-05-07 10:03:46 +0000

    Added function.js file

---
+++ function.js
@@ -0,0 +1,8 @@
+// function to add 2 input numbers.
+
+function addNumbers(){
+  var x = 2 //-----------------first input numbers
+  var y = 4 //-----------------second input numbers
+    console.log("The sum of two numbers is = "+ (x+y));
+}
+addNumbers();
