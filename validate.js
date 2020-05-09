d4795657b9cc23543b45d640fd04408c230b11d5
Priyansh Singh <priyanshsingh1401@gmail.com>
2020-05-09 19:17:00 +0000

    modified various files

---
+++ validate.js
@@ -0,0 +1,13 @@
+ function submitFn(){
+  var un = document.getElementById("uname").value
+  var pw = document.getElementById("pword").value
+
+    if(un =="" || pw ==""){
+    
+      alert("No blank inputs allowed!!!")
+      return false
+    }
+    else{
+      true
+    }
+ }
